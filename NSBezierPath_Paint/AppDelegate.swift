d81f8159d8b538c95399cc21a9a2abd7aa03820e
Saran Chockan <saranjitharunesh@gmail.com>
2018-02-24 23:27:14 -0600

    Initial Commit

---
+++ NSBezierPath_Paint/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSBezierPath_Paint
+//
+//  Created by Saranjitharunesh Chockan on 2/24/18.
+//  Copyright © 2018 Saranjitharunesh Chockan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NSBezierPath_Paint/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSBezierPath_Paint
+//
+//  Created by Saranjitharunesh Chockan on 2/24/18.
+//  Copyright © 2018 Saranjitharunesh Chockan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
